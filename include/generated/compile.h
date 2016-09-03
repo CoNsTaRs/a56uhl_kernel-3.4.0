132351226acf6412d798ba9b2a5157b5a927c089
CoNsTaRwU <CoNsTaRwU@gmail.com>
2016-09-03 19:58:33 +0800

    Initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Sep 3 00:11:12 CST 2016"
+#define LINUX_COMPILE_BY "constar"
+#define LINUX_COMPILE_HOST "X205TA"
+#define LINUX_COMPILER "gcc version 5.3.1 20160113 (Linaro GCC 5.3-2016.02) "
